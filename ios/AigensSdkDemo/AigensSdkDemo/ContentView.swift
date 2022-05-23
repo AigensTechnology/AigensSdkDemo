5a1f87973c5e54571ff49dc0d5fbd452e88b8971
Peter Liu <peter.liu@aigens.com>
2022-05-23 15:07:24 +0800

    init

---
+++ ios/AigensSdkDemo/AigensSdkDemo/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  AigensSdkDemo
+//
+//  Created by Peter on 23/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+  
+    var body: some View {
+        VStack(alignment: .leading) {
+            Button("Open SDK Link") {
+                
+                print("open clicked")
+                
+            }
+
+           
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
